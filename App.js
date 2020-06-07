aa3ff4d21e45d79d0320a7e3408fed806e22208c
M Younas <younaslook@outloo.com>
2020-06-07 17:38:34 +0800

    change

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+import { StyleSheet, Text, Image, View, SafeAreaView } from "react-native";
 
 export default function App() {
+  console.log("App executed");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello Android.</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
